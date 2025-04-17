af96724ac1c1e17337b594d6928902becf25ea77
Nikunj varshney <nikunjvarshney22jan@gmail.com>
2025-04-17 07:56:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -5,29 +5,25 @@ using namespace std;
 
 // } Driver Code Ends
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
-public:
+public: 
     void merge(vector<int>& arr, int l, int m, int r) {
-        int n1 = m - l + 1;
-        int n2 = r - m;
-
-        vector<int> L(n1), R(n2);
-
-        // Copy subarrays
-        for (int i = 0; i < n1; i++) {
+        int l1 = m - l + 1;
+        int l2 = r - m;
+        
+        vector<int> L(l1), R(l2);
+        
+        for (int i = 0; i < l1; i++) {
             L[i] = arr[l + i];
         }
-        for (int i = 0; i < n2; i++) {
-            R[i] = arr[m + 1 + i];
+        for (int j = 0; j < l2; j++) {
+            R[j] = arr[m + 1 + j];
         }
-
-        // Merge the two sorted subarrays
+        
         int i = 0, j = 0, k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {  // Change to >= for descending order
+        
+        while (i < l1 && j < l2) {
+            if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 i++;
             } else {
@@ -36,34 +32,31 @@ public:
             }
             k++;
         }
-
-        // Copy remaining elements of L[]
-        while (i < n1) {
+        
+        while (i < l1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-
-        // Copy remaining elements of R[]
-        while (j < n2) {
+        
+        while (j < l2) {
             arr[k] = R[j];
             j++;
             k++;
         }
     }
-
+    
     void mergeSort(vector<int>& arr, int l, int r) {
-        if (l >= r) return;
-
-        int mid = l + (r - l) / 2;
-        mergeSort(arr, l, mid);
-        mergeSort(arr, mid + 1, r);
-
-        merge(arr, l, mid, r);
+        if (l >= r) {
+            return;
+        }
+        
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 };
-
-
 
 
 //{ Driver Code Starts.
